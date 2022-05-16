6993aa579bc058296f73df34d97a656d04edd928
lee-aaron <lee.aaron.68@gmail.com>
2022-05-16 14:59:05 -0700

    Initial solana work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::{process::Command, str::FromStr};
+use solana_client::{
+    rpc_client::{self, RpcClient},
+    client_error::ClientError,
+};
+use solana_sdk::{
+    signature::Signer,
+    signer::keypair::Keypair,
+    pubkey::Pubkey
+};
+
+fn main() -> Result<(), ClientError> {
+    let mut cmd = Command::new("solana");
+    cmd.arg("address");
+    let addr = cmd.output().unwrap().stdout;
+    let key = std::str::from_utf8(&addr).unwrap();
+    println!("addr: {:?}", std::str::from_utf8(&addr).unwrap());
+
+    let client = RpcClient::new("http://localhost:8899");
+    let mut trim_addr = key.to_string();
+    trim_addr.truncate(key.len() - 1);
+    let pubkey = Pubkey::from_str(trim_addr.as_str()).unwrap();
+    let account = client.get_account(&pubkey)?;
+
+    println!("account: {:?}", account);
+
+    Ok(())
 }
