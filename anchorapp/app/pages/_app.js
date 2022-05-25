db4e0b66fd41aaf75e846743de0609d642bf7656
lee-aaron <lee.aaron.68@gmail.com>
2022-05-24 20:30:27 -0700

    Added frontend

---
+++ anchorapp/app/pages/_app.js
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import App from "../src/components/App";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <App>
+      <Component {...pageProps} />
+    </App>
+  )
+}
+
+export default MyApp;
